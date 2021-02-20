e474fafb524fa3e2345c3a94aa79a91d420e3a9b
Roman Badmaev <barv@inbox.ru>
2021-02-20 11:31:17 +0300

    Append files from https://code.google.com/archive/p/cpp-btree/

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cpp-btree
+
+git fork of https://code.google.com/archive/p/cpp-btree/
+
+
+also include dirictory is added with GCC9 warning suppress
---
+++ include/cppbtree.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wstrict-aliasing"
+#pragma GCC diagnostic ignored "-Wdeprecated-copy"
+#endif
+
+#include "../btree_map.h"
+#include "../btree_set.h"
+#include "../safe_btree_map.h"
+#include "../safe_btree_set.h"
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
